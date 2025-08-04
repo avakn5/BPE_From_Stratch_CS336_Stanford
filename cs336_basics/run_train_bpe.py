6a5df21089518014da8da109c05ba0ec8c106933
akouhana <akouhana@stanford.edu>
2025-08-04 14:31:55 -0400

    modifying the run_train_bpe file

--- cs336_basics/run_train_bpe.py
+++ cs336_basics/run_train_bpe.py
@@ -1,20 +1,24 @@
 from typing import List, Dict, Optional, Union, Tuple, Any
 import regex as re
 from collections import defaultdict
+import cProfile
 
 
-def read_file(input_path: str, max_chars: int = 10000) -> str:
-    """Read first max_chars from file"""
+def read_file(input_path: str, max_chars: int = 220000) -> str:
+    """Read first max_chars from file
+    """
     with open(input_path, 'r', encoding='utf-8') as file:
         content = file.read(max_chars)  # reads only first 10k characters
     return content
 
-def train_bpe(input_path: str, vocab_size: int = 256, special_tokens: list[str] = None) : 
- 
+
+
+def train_bpe(input_path: str, vocab_size: int = 256, special_tokens: list[str] = None, iteration: int  = 100) : 
     '''
     Trains a (byte-level) BPE tokenizer on a text. 
-    The function returns 
-        vocab: dict[int, bytes] and merges: list[tuple[bytes, bytes]]
+    Returns: 
+        - vocab: dict[int, bytes] which contains the tokenizer vocabulary.
+        - merges: list[tuple[bytes, bytes]] which contains the list of BPE merges produced from training.
     '''
 
     text = read_file(input_path)
@@ -28,18 +32,28 @@ def train_bpe(input_path: str, vocab_size: int = 256, special_tokens: list[str] 
     
     # STEP 2: pre-tokenize 
    
-    # a) split on special tokens
-    tokens = re.findall(r"<\|endoftext\|>|[^\s]+", text) # convert input text into pre-token
+    # a) split text into chunks
+    escaped_tokens = [re.escape(token) for token in special_tokens]
+    split_pattern = "|".join(escaped_tokens)
+    chunks = re.split(split_pattern, text)
+
+    # b) utf-8 encoding
+    tokens = []
+    PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+    for chunk in chunks:
+        tokens.extend(re.finditer(PAT, chunk)) # convert input text into pre-token
+    tokens = [strings.group(0) for strings in tokens]
+
     utf8_encoded = [token.encode("utf-8") for token in tokens] # rpz each token as utf-8 bytes.
     
-    # b) count occurences
+    # b) count word occurences
     char_dict = {}
     for word in utf8_encoded: 
         word_as_tuple = tuple(bytes([byte]) for byte in word)
         char_dict[word_as_tuple] = 1 if char_dict.get(word_as_tuple) is None else char_dict[word_as_tuple] + 1    
 
     # STEP 3: merge the most likely pairs 
-    
+
     # a) count pair occurences 
     pair_dict = defaultdict(int) 
     for word in char_dict:
@@ -48,17 +62,17 @@ def train_bpe(input_path: str, vocab_size: int = 256, special_tokens: list[str] 
     
     # b) merge them 
     merges = []
-    sorted_dict = sorted(pair_dict) #to change
+    sorted_dict = sorted(pair_dict.items(), key=lambda item: item[1], reverse = True)
 
-    for i in range(10):
-        pairmerge = sorted_dict[-i]
+    for i in range(iteration):
+        pairmerge = sorted_dict[-i][0]
         pairmerge = b''.join(pairmerge) #from tuple to pair 
         merges.append(pairmerge) 
         vocab_size += 1  
-    
-    
-    print(vocab_size)
+        
     print(merges)
+        
     return pair_dict, merges
 
+cProfile.run('re.compile("foo|bar")')
 train_bpe("/Users/akouhana/CS336/assignment1-basics/data/TinyStoriesV2-GPT4-train.txt")
