29e351b8fbf628b81956cf2cdcb15c5f31bbd4c9
akouhana <akouhana@stanford.edu>
2025-08-05 13:31:05 -0400

    update the train_bpe

--- cs336_basics/run_train_bpe.py
+++ cs336_basics/run_train_bpe.py
@@ -11,12 +11,12 @@ def read_file(input_path: str, max_chars: int = 220000) -> str:
     """Read first max_chars from file
     """
    
-    with open(input_path, 'r') as file:
+    with open(input_path, 'r', encoding= "utf-8") as file:
         content = file.read(max_chars)  # reads only first max_char characters (prevents overloading memory)
     return content
 
 
-def train_bpe(input_path: str, vocab_size: int = 256, special_tokens: list[str] = None, max_iteration: int  = 100) : 
+def train_bpe(input_path: str, vocab_size: int, special_tokens: list[str] = None) : 
     '''
     Trains a (byte-level) BPE tokenizer on any given text. 
     
@@ -25,6 +25,9 @@ def train_bpe(input_path: str, vocab_size: int = 256, special_tokens: list[str] 
         - vocab_size: int A positive integer that defines the maximum final vocabulary size (including the initial byte vocabulary, vocabulary items produced from merging, and any special tokens).
         - special_tokens: list[str] A list of strings to add to the vocabulary. 
 
+    Creates: 
+        - word_frequency_dict : contains the frequency of each word and is useful to count pair frequencies (which are afterwards stored in the byte_pair_frequency dict). The merged pairs are modified in place inside the word_frequnecy_dict.
+        - pair_frequency_dict : temprary helper which contains the frequency of each pair. It is recomputed at each iteration. 
     Returns: 
         - vocab: dict[int, bytes] which contains the tokenizer vocabulary.
         - merges: list[tuple[bytes, bytes]] which contains the list of BPE merges produced from training.
@@ -35,6 +38,8 @@ def train_bpe(input_path: str, vocab_size: int = 256, special_tokens: list[str] 
     if special_tokens is None: 
         special_tokens = ["<|endoftext|>"] 
     
+    current_vocab_size = 256
+
     # STEP 1: removing special characters
     text_content = text_content.lower()
     
@@ -65,43 +70,47 @@ def train_bpe(input_path: str, vocab_size: int = 256, special_tokens: list[str] 
     #encodes each pretoken in utf-8 encoding.
     utf8_encoded = [pretoken.encode("utf-8") for pretoken in pretokens] 
     
-    # c) count word occurences
-    word_frequency_dict = {}
+    # c) creating the word frequency dict
+    
+    word_frequency_dict = {}    #  count word occurences
     for word in utf8_encoded: 
-        # creating a dictionary containing words stored as tuples and their frequency in the text.
-        word_as_tuple = tuple(bytes([byte]) for byte in word)
-        # fetching the frequency of the word in the text. 0 being the default value, if the word is not found in the text. 
-        word_frequency_dict[word_as_tuple] = word_frequency_dict.get(word_as_tuple, 0) + 1 
-
+            # creating a dictionary containing words stored as tuples and their frequency in the text.
+            word_as_tuple = tuple(bytes([byte]) for byte in word)
+            # fetching the frequency of the word in the text. 0 being the default value, if the word is not found in the text. 
+            word_frequency_dict[word_as_tuple] = word_frequency_dict.get(word_as_tuple, 0) + 1 
+            
     # STEP 3: merge the most likely pairs 
-
-    # a) count pair occurences 
-    byte_pair_frequency = defaultdict(int) 
     
-    for word in word_frequency_dict:
-        for consecutive_byte_pair in zip(word, word[1:]): #looking for every pair of adjacent characters.
-            # adding the pair frequency to the byte_pair_frequency dictionary. 
-            byte_pair_frequency[consecutive_byte_pair] +=  word_frequency_dict[word]
-
-    # b) merge them 
-    merges = []
-    sorted_dict = sorted(byte_pair_frequency.items(), key=lambda item: item[1], reverse = True) # sort by frequency in increasing order
-
-    #while current_vocab_size < vocab_size
-    for i in range(max_iteration): 
-        most_frequent_pair = sorted_dict[-i][0] # fect the most frequent pair
-        most_frequent_pair = b''.join(most_frequent_pair) # merge the most frequent pair to be a string instead of a tuple
-        merges.append(most_frequent_pair) 
-        vocab_size += 1  #update the vocabulary size.
-         
-    # c) constructing the tokenizer vocabulary 
+    pair_frequency_dict = defaultdict(int) # count pair occurences 
+    merges = [] # merge them 3
     vocabulary = {} 
-    for id in enumerate(merges):
-        vocabulary[id] = id
     
-    assert len(vocabulary) == len(set(vocabulary)) # vocabulary should be a set
+    while current_vocab_size < vocab_size:
+        id = 0
+        
+        # a) compute byte_pair_frequency from word_frequency_dict.
+        for word in word_frequency_dict:
+            for consecutive_byte_pair in zip(word, word[1:]): #looking for every pair of adjacent characters.
+                # adding the pair frequency to the byte_pair_frequency dictionary. 
+                pair_frequency_dict[consecutive_byte_pair] +=  word_frequency_dict[word] #here we get the frequency of each pair across the chunk.
+        
+        # b) find the most frequent pair
+        sorted_dict = sorted(pair_frequency_dict.items(), key=lambda item: item[1], reverse = True) # sort by frequency in descending order
+        most_frequent_pair = sorted_dict[0][0] # fect the most frequent pair
+        most_frequent_pair = b''.join(most_frequent_pair) # merge the most frequent pair to be a string instead of a tuple
+
+        # c) update word_frequency_dict : merge the most frequent typle with a single token. 
+        for word in word_frequency_dict:
+            for consecutive_byte_pair in zip(word, word[1:]): #looking for every pair of adjacent characters.
+                if consecutive_byte_pair == most_frequent_pair:
+                    print(consecutive_byte_pair)
+                    break
+                
+        # d) append the merged pair to the merges list.
+
+        # e) Recompute byte_pair_frequency from the newly updated word_frequency_dict.
 
     return vocabulary, merges
 
 # cProfile.run('re.compile("foo|bar")')
-train_bpe("/Users/akouhana/CS336/assignment1-basics/data/TinyStoriesV2-GPT4-train.txt")
+train_bpe("/Users/akouhana/CS336/assignment1-basics/data/TinyStoriesV2-GPT4-train.txt", 500)
