f986e28c843a91af1569308a4f8cde0b6c75e54b
akouhana <akouhana@stanford.edu>
2025-08-05 12:30:49 -0400

    run train bpe comments update

--- cs336_basics/run_train_bpe.py
+++ cs336_basics/run_train_bpe.py
@@ -7,8 +7,10 @@ from pretokenization_example import find_chunk_boundaries
 PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""" #Pre-tokenization pattern
 
 def read_file(input_path: str, max_chars: int = 220000) -> str:
+   
     """Read first max_chars from file
     """
+   
     with open(input_path, 'r') as file:
         content = file.read(max_chars)  # reads only first max_char characters (prevents overloading memory)
     return content
@@ -16,7 +18,7 @@ def read_file(input_path: str, max_chars: int = 220000) -> str:
 
 def train_bpe(input_path: str, vocab_size: int = 256, special_tokens: list[str] = None, max_iteration: int  = 100) : 
     '''
-    Trains a (byte-level) BPE tokenizer on a text.  
+    Trains a (byte-level) BPE tokenizer on any given text. 
     
     Args:
         - input_path: str Path to a text file with BPE tokenizer training data.
@@ -28,54 +30,69 @@ def train_bpe(input_path: str, vocab_size: int = 256, special_tokens: list[str] 
         - merges: list[tuple[bytes, bytes]] which contains the list of BPE merges produced from training.
     '''
     
-    text_content = read_file(input_path)
+    text_content = read_file(input_path) 
     
     if special_tokens is None: 
         special_tokens = ["<|endoftext|>"] 
     
     # STEP 1: removing special characters
     text_content = text_content.lower()
-    cleaned_text = re.sub(r'[!@#$\^*]', ' ', text_content)
     
-    # STEP 2: pre-tokenize 
-   
-    # a) split text into chunks
+    #substitutes special characters with space in the text_content
+    cleaned_text = re.sub(r'[!@#$\^*]', ' ', text_content) 
+    
+    # STEP 2: pre-tokenize : split text into chunks
+    
+    # the special token |<endoftext>| is "escaped" meaning that it is set aside in the tokenization scheme 
+    # otherwise  |<>| would be interpreted as a metacharacter, and not used as a text delimiter. 
     escaped_tokens = [re.escape(token) for token in special_tokens]
+   
+    # creates the split pattern  
     split_pattern = "|".join(escaped_tokens)
-    chunks = re.split(split_pattern, cleaned_text)
-
+    
+    # re.split() breaks the text at each occurrence of the pattern
+    chunks = re.split(split_pattern, cleaned_text) 
+    
     # b) utf-8 encoding
     pretokens = []
     for chunk in chunks:
-        pretokens.extend(re.finditer(PAT, chunk)) # convert input text into pre-token
-    pretokens = [strings.group(0) for strings in pretokens]
+        #chunks are pre-tokenized (using PAT, the pre-tokenization pattern), and added to the end of the list pretokens.
+        pretokens.extend(re.finditer(PAT, chunk)) 
 
-    utf8_encoded = [pretoken.encode("utf-8") for pretoken in pretokens] # rpz each token as utf-8 bytes.
+    #extracting only the pretokens from the regex object
+    pretokens = [strings.group(0) for strings in pretokens] 
+
+    #encodes each pretoken in utf-8 encoding.
+    utf8_encoded = [pretoken.encode("utf-8") for pretoken in pretokens] 
     
     # c) count word occurences
     word_frequency_dict = {}
     for word in utf8_encoded: 
+        # creating a dictionary containing words stored as tuples and their frequency in the text.
         word_as_tuple = tuple(bytes([byte]) for byte in word)
-        word_frequency_dict[word_as_tuple] = word_frequency_dict.get(word_as_tuple,0) + 1 
+        # fetching the frequency of the word in the text. 0 being the default value, if the word is not found in the text. 
+        word_frequency_dict[word_as_tuple] = word_frequency_dict.get(word_as_tuple, 0) + 1 
 
     # STEP 3: merge the most likely pairs 
 
     # a) count pair occurences 
     byte_pair_frequency = defaultdict(int) 
+    
     for word in word_frequency_dict:
-        for consecutive_byte_pair in zip(word, word[1:]):
+        for consecutive_byte_pair in zip(word, word[1:]): #looking for every pair of adjacent characters.
+            # adding the pair frequency to the byte_pair_frequency dictionary. 
             byte_pair_frequency[consecutive_byte_pair] +=  word_frequency_dict[word]
 
     # b) merge them 
     merges = []
-    sorted_dict = sorted(byte_pair_frequency.items(), key=lambda item: item[1], reverse = True) 
+    sorted_dict = sorted(byte_pair_frequency.items(), key=lambda item: item[1], reverse = True) # sort by frequency in increasing order
 
     #while current_vocab_size < vocab_size
     for i in range(max_iteration): 
-        most_frequent_pair = sorted_dict[-i][0] 
-        most_frequent_pair = b''.join(most_frequent_pair) #from tuple to string
+        most_frequent_pair = sorted_dict[-i][0] # fect the most frequent pair
+        most_frequent_pair = b''.join(most_frequent_pair) # merge the most frequent pair to be a string instead of a tuple
         merges.append(most_frequent_pair) 
-        current_vocab_size += 1  
+        vocab_size += 1  #update the vocabulary size.
          
     # c) constructing the tokenizer vocabulary 
     vocabulary = {} 
